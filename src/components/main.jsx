68666fe014b3efb027d947bcd698c562b9689e49
Dulguun <dulguun@Dulguuns-MacBook-Air.local>
2024-02-29 21:50:53 +0800

    desktop version done

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import audioPhile from '../assets/client-audiophile.svg'
+import clientDatabiz from '../assets/client-databiz.svg'
+import clientMaker from '../assets/client-maker.svg'
+import clientMeet from '../assets/client-meet.svg'
+import imageDesktop from '../assets/image-hero-desktop.png'
+import imageMobile from '../assets/image-hero-mobile.png'
 
 const Main = () => {
     return (
-        <>
-            <div>
-                <h1>Make remote work</h1>
+        <section>
+            <div className='sec-1'>
+                <h1>Make </h1>
+                <h1>remote work</h1>
                 <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
                 <button>Learn more</button>
-                <div>
-
+                <div className='clients'>
+                    <img src={clientDatabiz} alt="client" />
+                    <img src={audioPhile} alt="client" />
+                    <img src={clientMeet} alt="client" />
+                    <img src={clientMaker} alt="client" />
                 </div>
             </div>
             <div>
-
+                <img id='HeroDesktop' src={imageDesktop} alt="hero" />
             </div>
-        </>
+        </section>
 
     )
 }
